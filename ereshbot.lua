d4e9746ddb41d7e478d2b80a1e21a8223f17228a
Natã Botelho <nbotelho789@hotmail.com>
2020-05-11 23:56:36 -0300

    move command added

--- ereshbot.lua
+++ ereshbot.lua
@@ -12,53 +12,64 @@ math.random(); math.random(); math.random()
 
 --recebe uma mensagem do canal
 client:on('messageCreate', function(message)
-    answered = false
+    local msgPrefix = string.sub(message.content, 1, 1)
 
-    if(message.content == prefix..'help')   then
-        message.channel:send('Lista de comandos:')
-        message.channel:send('>D4: lança um dado de 4 faces')
-        message.channel:send('>D6: lança um dado de 6 faces')
-        message.channel:send('>D8: lança um dado de 8 faces')
-        message.channel:send('>D10: lança um dado de 10 faces')
-        message.channel:send('>D12: lança um dado de 12 faces')
-        message.channel:send('>D20: lança um dado de 20 faces')
+    if(msgPrefix == prefix) then
+        local answered = false
+        local msgContent = string.lower(string.sub(message.content, 2))
+        local msgCmd, msgArg = string.match(msgContent, '(%S+) (.*)')
+        msgCmd = msgCmd or msgContent
+        local msgMember = message.member
+        local msgAuthor = message.author
+        local msgGuild = message.guild
+        local msgMentioned = message.mentionedUsers
+        
+        if(msgCmd == 'help')   then
+            message.channel:send('Lista de comandos:\n>D4: lança um dado de 4 faces\n>D6: lança um dado de 6 faces\n>D8: lança um dado de 8 faces\n>D10: lança um dado de 10 faces\n>D12: lança um dado de 12 faces\n>D20: lança um dado de 20 faces\n\nbot feito por natã/wolfgan/bnegão :D')
+            answered = true
 
-        message.channel:send('>bot feito por natã/wolfgan/bnegão :D')
-        answered = true
-
-    elseif(answered == false) then
-        result = dice(message)
-        if(type(result) == "number")  then 
-            if(message.member) then
-                message.channel:send(message.member.nickname..' tirou: '..result)
-            else
-                message.channel:send(message.author.name..' tirou: '..result)
+        elseif(msgCmd == 'move')    then
+            if(#msgMentioned == 1)  then
+                local mentionedMember = msgGuild:getMember(msgMentioned[1][1])
+                mentionedMember:setVoiceChannel(msgMember.voiceChannel.id)
+                message.channel:send(mentionedMember.nickname..' foi movido para o canal '..msgMember.voiceChannel.name..' por '..msgMember.nickname)
+                answered = true
             end
+
+        elseif(answered == false) then
+            local result = dice(msgCmd)
+            if(type(result) == "number")  then 
+                if(msgMember and msgMember.nickname ~= nil) then
+                    message.channel:send(msgMember.nickname..' tirou: '..result)
+                else
+                    message.channel:send(msgAuthor.name..' tirou: '..result)
+                end
+                answered = true
+            end
+
+        elseif (answered == false) then
+            message.channel:send('Comando não encontrado! Digite >help para ver a lista de comandos')
             answered = true
         end
-
-    elseif (string.sub(message.content, 1, 1) == prefix and answered == false) then
-        message.channel:send('Comando não encontrado! Digite >help para ver a lista de comandos')
-        answered = true
     end
 end)
 
 --função que retorna o valor do dado
 dice = function(message)
-    if (message.content == prefix..'d4' or message.content == prefix..'D4') then
+    if (message == 'd4') then
         return(math.random(1, 4))
-    elseif (message.content == prefix..'d6' or message.content == prefix..'D6') then
+    elseif (message =='d6') then
         return(math.random(1, 6))
-    elseif (message.content == prefix..'d8' or message.content == prefix..'D8') then
+    elseif (message == 'd8') then
         return(math.random(1, 8))
-    elseif (message.content == prefix..'d10' or message.content == prefix..'D10') then
+    elseif (message == 'd10') then
         return(math.random(1, 10))
-    elseif (message.content == prefix..'d12' or message.content == prefix..'D12') then
+    elseif (message == 'd12') then
         return(math.random(1, 12))
-    elseif (message.content == prefix..'d20' or message.content == prefix..'D20') then
+    elseif (message == 'd20') then
         return(math.random(1, 20))
     else    return "error"
     end
-end
+end 
 
 client:run("Bot "..token)
