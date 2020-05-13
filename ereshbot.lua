74c6d91267a01a5c0747a46d83aadaa3fd0289f0
Natã Botelho <nbotelho789@hotmail.com>
2020-05-13 00:08:23 -0300

    welcome and bye messages added besides other commands (see in >help)

--- ereshbot.lua
+++ ereshbot.lua
@@ -10,39 +10,82 @@ file:close()
 math.randomseed(os.time())
 math.random(); math.random(); math.random()
 
+client:on('ready', function()
+    client:setGame('Fate/Grand Order')
+    print("Logged as "..client.user.username)
+end)
+
+client:on('memberJoin', function(member)
+    member.guild.systemChannel:send('Fala fiote '..member.name)
+end)
+
+client:on('memberLeave', function(member)
+    member.guild.systemChannel:send('Valeeeeu falooooou '..member.name)
+end)
+
+client:on('memberUpdate', function(member)
+    if(string.lower(member.highestRole.name) == 'inclusão social')  then
+        member.guild.systemChannel:send(member.name..' entrou no programa de cotas!')
+    end
+end)
+
 --recebe uma mensagem do canal
 client:on('messageCreate', function(message)
     local msgPrefix = string.sub(message.content, 1, 1)
+
+    if(message.content == 'dadinho')    then
+        message.channel:send('dadinho é o krl meu nome agr é zé pequeno porra!')
+    end
 
     if(msgPrefix == prefix) then
         local answered = false
         local msgContent = string.lower(string.sub(message.content, 2))
         local msgCmd, msgArg = string.match(msgContent, '(%S+) (.*)')
         msgCmd = msgCmd or msgContent
-        local msgMember = message.member
-        local msgAuthor = message.author
-        local msgGuild = message.guild
-        local msgMentioned = message.mentionedUsers
+        local member = message.member
+        local author = message.author
+        local guild = message.guild
+        local mentioned = message.mentionedUsers
         
         if(msgCmd == 'help')   then
-            message.channel:send('Lista de comandos:\n>D4: lança um dado de 4 faces\n>D6: lança um dado de 6 faces\n>D8: lança um dado de 8 faces\n>D10: lança um dado de 10 faces\n>D12: lança um dado de 12 faces\n>D20: lança um dado de 20 faces\n\nbot feito por natã/wolfgan/bnegão :D')
+            message.channel:send('Lista de comandos:\nDados:\n>D4: lança um dado de 4 faces\n>D6: lança um dado de 6 faces\n>D8: lança um dado de 8 faces\n>D10: lança um dado de 10 faces\n>D12: lança um dado de 12 faces\n>D20: lança um dado de 20 faces\n\n>Discordia: Github Discordia\n>MemberCount: número de membros no server\n>Move @(usuario): move o usuário mencionado para o seu canal\n>Region: mostra a região atual do servidor\n\nbot feito por natã/wolfgan/bnegão :D')
             answered = true
 
-        elseif(msgCmd == 'move')    then
-            if(#msgMentioned == 1)  then
-                local mentionedMember = msgGuild:getMember(msgMentioned[1][1])
-                mentionedMember:setVoiceChannel(msgMember.voiceChannel.id)
-                message.channel:send(mentionedMember.nickname..' foi movido para o canal '..msgMember.voiceChannel.name..' por '..msgMember.nickname)
+        elseif(msgCmd == 'move' and guild)    then
+            if(#mentioned == 1)  then
+                local mentionedMember = guild:getMember(mentioned[1][1])
+                mentionedMember:setVoiceChannel(member.voiceChannel.id)
+                message.channel:send(mentionedMember.nickname..' foi movido para o canal '..member.voiceChannel.name..' por '..member.nickname)
                 answered = true
             end
 
+        elseif(msgCmd == 'discordia')    then
+            message.channel:send('https://github.com/SinisterRectus/Discordia')
+            answered = true
+
+        elseif(msgCmd == 'membercount' and guild)    then
+            message.channel:send('O servidor está atualmente com '..guild.totalMemberCount..' membros')
+            answered = true
+        
+        elseif(msgCmd == 'region' and guild)    then
+            message.channel:send('Região do servidor: '..guild.region)
+            answered = true
+
+        elseif(msgCmd == 'ban' and guild)    then
+            if(#mentioned == 1)  then
+                local mentionedMember = guild:getMember(mentioned[1][1])
+                message.channel:send('Banindo '..mentionedMember.nickname..' em 10 segundos')
+                answered = true
+            end
+            
+            
         elseif(answered == false) then
             local result = dice(msgCmd)
             if(type(result) == "number")  then 
-                if(msgMember and msgMember.nickname ~= nil) then
-                    message.channel:send(msgMember.nickname..' tirou: '..result)
+                if(member and member.nickname ~= nil) then
+                    message.channel:send(member.nickname..' tirou: '..result)
                 else
-                    message.channel:send(msgAuthor.name..' tirou: '..result)
+                    message.channel:send(author.name..' tirou: '..result)
                 end
                 answered = true
             end
